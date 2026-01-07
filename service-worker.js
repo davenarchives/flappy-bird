1295ff8eebaa6511ad1957c2bd9140cb4273b5dd
davenarchives <daventheabyss@gmail.com>
2026-01-07 20:23:45 +0800

    force service worker refresh to apply UI changes

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "happy-bird-cache-v1";
+const CACHE_NAME = "happy-bird-cache-v2";
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
@@ -12,6 +12,20 @@ const FILES_TO_CACHE = [
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
 
